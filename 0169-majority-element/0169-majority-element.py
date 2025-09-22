f5feb920d7349504c33b060a9133d2c6cd206b21
Prabhat Kumar <116169998+prabhatadvait@users.noreply.github.com>
2025-09-22 08:57:27 +0530

    0169-MAJORITY-ELEMENT Stats: Time: 7 ms (55.96%), Space: 19.3 MB (87.03%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,8 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        from collections import Counter
-        a = Counter(nums)
-        n=len(nums)
-        for key,value in a.items():
+        # from collections import Counter
+        # a = Counter(nums)
+        # n=len(nums)
+        # for key,value in a.items():
+        #     if value > n//2:
+        #         return key
+
+        my_dict = {}
+        for i in nums:
+            my_dict[i] = my_dict.get(i,0)+1
+        n = len(nums)
+        for key,value in my_dict.items():
             if value > n//2:
                 return key
