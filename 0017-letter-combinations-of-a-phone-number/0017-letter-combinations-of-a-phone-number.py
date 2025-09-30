4d5e6adea6f723c1d92ba33c72a3847c9070fbd8
Prabhat Kumar <116169998+prabhatadvait@users.noreply.github.com>
2025-09-30 12:26:50 +0530

    0017-LETTER-COMBINATIONS-OF-A-PHONE-NUMBER Stats: Time: 0 ms (100%), Space: 17.8 MB (42.36%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,4 +1,35 @@
+# from typing import List
+
+# class Solution:
+#     def letterCombinations(self, digits: str) -> List[str]:
+#         if not digits:
+#             return []
+
+#         mapping = {
+#             "2": "abc",
+#             "3": "def",
+#             "4": "ghi",
+#             "5": "jkl",
+#             "6": "mno",
+#             "7": "pqrs",
+#             "8": "tuv",
+#             "9": "wxyz"
+#         }
+
+#         ans = []
+
+#         def backtrack(index: int, current: str):
+#             if index == len(digits):
+#                 ans.append(current)
+#                 return
+
+#             for ch in mapping[digits[index]]:
+#                 backtrack(index + 1, current + ch)
+
+#         backtrack(0, "")
+#         return ans
 from typing import List
+from itertools import product
 
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
@@ -16,15 +47,8 @@ class Solution:
             "9": "wxyz"
         }
 
-        ans = []
+        # Build the list of letters for each digit
+        groups = [mapping[d] for d in digits]
 
-        def backtrack(index: int, current: str):
-            if index == len(digits):
-                ans.append(current)
-                return
-
-            for ch in mapping[digits[index]]:
-                backtrack(index + 1, current + ch)
-
-        backtrack(0, "")
-        return ans
+        # Cartesian product of all groups
+        return ["".join(p) for p in product(*groups)]
