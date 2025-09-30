33b87a9b5d262bfb46d67279bbb79a28d86571d6
Prabhat Kumar <116169998+prabhatadvait@users.noreply.github.com>
2025-09-30 12:16:35 +0530

    0017-LETTER-COMBINATIONS-OF-A-PHONE-NUMBER Stats: Time: 3 ms (5.54%), Space: 17.8 MB (69.37%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,30 @@
+from typing import List
+
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        if not digits:
+            return []
+
+        mapping = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "pqrs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        ans = []
+
+        def backtrack(index: int, current: str):
+            if index == len(digits):
+                ans.append(current)
+                return
+
+            for ch in mapping[digits[index]]:
+                backtrack(index + 1, current + ch)
+
+        backtrack(0, "")
+        return ans
