adab66277760055fa0d5646177d030d4cc065c4d
Prabhat Kumar <116169998+prabhatadvait@users.noreply.github.com>
2025-10-23 11:03:27 +0530

    3768-CHECK-IF-DIGITS-ARE-EQUAL-IN-STRING-AFTER-OPERATIONS-I Stats: Time: 52 ms (73.12%), Space: 18 MB (26.37%) - LeetHub

--- 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.py
+++ 3768-check-if-digits-are-equal-in-string-after-operations-i/3768-check-if-digits-are-equal-in-string-after-operations-i.py
@@ -3,7 +3,7 @@ class Solution:
         n=len(s)
         digit = [int(ch) for ch in s]
         for i in range(1,n-1):
-            for j in range(n-i):
+            for j in range(n-1):
                 d1 = digit[j]
                 d2 = digit[j+1]
                 digit[j] = (d1+d2)%10
